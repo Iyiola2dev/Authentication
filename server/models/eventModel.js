ec1053639f87aba4e15a9a67445c188b4cc99aae
Lexicon <iyiolad21@gmail.com>
2024-10-06 09:06:24 +0100

    Refactor userController.js to use "userid" instead of "useer" in createEvent function

--- server/models/eventModel.js
+++ server/models/eventModel.js
@@ -2,19 +2,20 @@ import { Schema, model } from "mongoose";
 
 const eventSchema = new Schema({
   title: { type: String, required: true },
-  description: { type: String }, 
+  description: { type: String },
   location: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
+
+  // createdAt: { type: Date, default: Date.now },
   imageUrl: { type: String, required: true },
-  startDateTime: { type: Date, default: Date.now },
-  endDateTime: { type: Date, default: Date.now },
+  startDateTime: { type: Date, required: true },  
+  endDateTime: { type: Date },
   price: { type: String },
   isFree: { type: Boolean, default: false },
   url: { type: String },
   category: { type: Schema.Types.ObjectId, ref: "Category" }, // Reference to Category model
   organizer: { type: Schema.Types.ObjectId, ref: "User" }, // Reference to User model
-});
+}, { timestamps: true },);
 
-const Ievent = model("ievents", eventSchema);
+const Ievents = model("Ievents", eventSchema);
 
-export default Ievent;
+export default Ievents;
